b457169cfb8c719df4550ba2e4e4e67f95fb8fb5
babi-barbato <barbara.oliveira@sptech.school>
2024-08-15 16:35:23 -0300

    feat: calendario

--- vivene-velas-frontend/src/routes.js
+++ vivene-velas-frontend/src/routes.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import Dashboard from "./pages/dashboard/Dashboard"
 import Estoque from "./pages/estoque/Estoque"
 import Vela from "./pages/vela/Vela"
+import Calendario from "./pages/calendario/Calendario"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -14,6 +15,7 @@ function Rotas() {
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/vela" element={<Vela />} />
                     <Route path="/estoque" element={<Estoque />} />
+                    <Route path="/calendario" element={<Calendario />} />
                 </Routes>
             </BrowserRouter>
         </>
