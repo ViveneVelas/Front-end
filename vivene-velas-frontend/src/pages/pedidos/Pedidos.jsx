3cae726cfcfe9bba973c8ad1fb494ed7c01e5ba7
Isa14-noronha <isabela.noronha@sptech.school>
2024-09-12 10:08:18 -0300

    Feat: Finalzação da tela de registro de pedidos

--- vivene-velas-frontend/src/pages/pedidos/Pedidos.jsx
+++ vivene-velas-frontend/src/pages/pedidos/Pedidos.jsx
@@ -1,22 +1,47 @@
 import React, { useState } from 'react';
-import CardPedido from '../../components/card-pedido/CardPedido';
+import style from './Pedidos.module.css'
 import Sidebar from '../../components/sidebar/Sidebar';
 import BarraData from '../../components/barradata/BarraData';
 
 const Pedidos = () => {
 
-    // const[barraAberta, setBarraAberta] = useState(false)
-
-    // const pedidosAbertos = () => setAberto(!aberto)
 
 
     return (
         <>
             <Sidebar />
-            
-            <div>
-                <BarraData />
-           
+
+            <div className={style['div-global']}>
+
+                <div className={style['div-topo-busca-filtro']}>
+                    <div className={style['div-caixa-busca']}>
+
+                        <div className={style['div-busca-input']}>
+                            <input type="text" placeholder='Buscar vela pelo nome' className={style['input']} />
+                            <i className=' bi bi-search'></i>
+                        </div>
+
+                        <div >
+                            <button className={style['botao-filtrar']}>
+                                <i class="bi bi-filter"></i>
+                                Filtrar
+                            </button>
+                        </div>
+
+                    </div>
+
+                    <button className={style['botao-adicionar-pedido']}>
+                        <i class="bi bi-plus"></i>
+                        Adicionar Pedido</button>
+
+                </div>
+
+                <div className={style['dados-pedido']}>
+
+                    <BarraData />
+
+                </div>
+
             </div>
 
 
