9a45464f96f8a7a67c181d050068f14cab05c886
Thyagoretuci12 <thyago.retuci@sptech.school>
2024-10-10 17:36:13 -0300

    feat: adicionando tabela de clientes na dashboard

--- vivene-velas-frontend/src/components/tabelavelas/Tabelavelas.jsx
+++ vivene-velas-frontend/src/components/tabelavelas/Tabelavelas.jsx
@@ -1,39 +1,60 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Tabelavelas = () => {
-    const dados = [
-        { nome: 'Carlos Andrade', vendas: 20 },
-        { nome: 'Carlos Andrade', vendas: 20 },
-        { nome: 'Carlos Andrade', vendas: 20 },
-        { nome: 'Carlos Andrade', vendas: 20 },
-        { nome: 'Carlos Andrade', vendas: 20 },
-        { nome: 'Carlos Andrade', vendas: 20 },
-        { nome: 'Carlos Andrade', vendas: 20 }
-    ];
+    const [dados, setDados] = useState([]);
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const pedidoResponse = await axios.get('http://localhost:8080/pedidos/top-cinco-pedidos', {
+            headers: {
+              'accept': '*/*',
+            },
+          });
+    
+          const responseData = pedidoResponse.data;
+
+          // Coloca o objeto dentro de um array
+          const dataArray = Array.isArray(responseData) ? responseData : [responseData];
+          
+          setDados(dataArray);
+          console.log(pedidoResponse.data)
+        } catch (error) {
+          console.error('Erro ao buscar os dados:', error);
+        }
+      };
+    
+      fetchData();
+    }, []);
 
     return (
         <>
             <div className="table-container card">
-                <h5 className="">Velas em Vencimento</h5>
+                <h5 className="">Pedidos próximos do vencimento</h5>
                 <table className="tabela-vendas">
                     <thead>
                         <tr>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Quantidade</th>
                             <th scope="col">Data</th>
+                            <th scope="col">Nome do Cliente</th>
+                            <th scope="col">Quantidade de Velas</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Vela de Morango</td>
-                            <td>8</td>
-                            <td>2016-05-25</td>
-                        </tr>
-                        <tr>
-                            <td>Vela de Chocolate</td>
-                            <td>3</td>
-                            <td>2014-12-05</td>
-                        </tr>
+                        {Array.isArray(dados) && dados.length > 0 ? (
+                            dados.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.dataPedido}</td>
+                                    <td>{item.nomeCliente}</td>
+                                    <td>{item.qtdVelas}</td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="3">Nenhum dado disponível</td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
