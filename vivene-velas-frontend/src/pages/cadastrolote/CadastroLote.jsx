dcf7b1fdd23f4f42148493f3def8dbadc2bf2128
Isa14-noronha <isabela.noronha@sptech.school>
2024-10-28 18:16:44 -0300

    Style: Mudanças na página de cadastro de lote - ajustes no text area, e layout na página

--- vivene-velas-frontend/src/pages/cadastrolote/CadastroLote.jsx
+++ vivene-velas-frontend/src/pages/cadastrolote/CadastroLote.jsx
@@ -8,8 +8,6 @@ import style from './CadastroLote.module.css'
 
 const CadastroLotes = () => {
 
-    const [image, setImage] = useState(null);
-
     const navigate = useNavigate();
 
 
@@ -17,91 +15,83 @@ const CadastroLotes = () => {
         navigate('/vela');
     };
 
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setImage(reader.result);
-            };
-            reader.readAsDataURL(file);
-        }
-    };
 
 
     return (
         <>
             <Sidebar />
 
-            <div>
+            <div className={style["div-pagina"]}>
 
-                <div>
-                    <h3>Cadastro de Lote</h3>
+                <div className={style["div-info-pagina"]}>
 
-                </div>
+                    <div>
+                        <h4>Cadastro de Lote</h4>
 
-
-                <div className="div-cad-lote">
-
-
-                    <div className="form-group image-upload">
-                        <input
-                            type="file"
-                            id="ipt_image"
-                            className="form-input"
-                            accept="image/*"
-                            onChange={handleImageChange}
-                            style={{ display: 'none' }}
-                        />
-                        <label htmlFor="ipt_image" className="form-card">
-
-                        </label>
                     </div>
 
 
-                    <div className='div-imputs-form'>
+                    <div className={style["div-cad-lote"]}>
 
-                        <div className={style["div-inputs"]}>
 
-                            <select className={style["select-optional"]} name="" id="">
-                                <option disabled selected>Selecione uma vela </option>
-                                <option value="Vela de Cacau">Vela de Cacau</option>
-                                <option value="Vela de Cacau">Vela de Cacau</option>
-                                <option value="Vela de Cacau">Vela de Cacau</option>
+                        <div className="form-group image-upload">
+                            <input
+                                type="file"
 
-                            </select>
+                                id="ipt_image"
+                                className="form-input"
+                                style={{ display: 'none' }}
+                            />
+                            <label htmlFor="ipt_image" className="form-card">
 
-                            <Input valor={"Quantidade de velas"} />
-
-                            <div className={style["div-check-box"]}>
-
-                                <CheckBox valor={"Casa"} />
-                                <CheckBox valor={"Estúdio"} />
-
-                            </div>
-
-                            <InputDesabilitado valor={"Tamanho da vela"} />
-                            <InputDesabilitado valor={"Valor"} />
-
-                            <div className={style["div-text-area"]}>
-                                <textarea name="" id="" disabled className={style["text-area"]}>fQIFGqo</textarea>
-                            </div>
-
+                            </label>
                         </div>
+
+
+                        <div className={style["div-inputs-form"]}>
+
+                            <div className={style["div-inputs"]}>
+
+                                <select className={style["select-optional"]} name="" id="">
+                                    <option disabled selected>Selecione uma vela </option>
+                                    <option value="Vela de Cacau">Vela de Cacau</option>
+                                    <option value="Vela de Cacau">Vela de Cacau</option>
+                                    <option value="Vela de Cacau">Vela de Cacau</option>
+
+                                </select>
+
+                                <Input valor={"Quantidade de velas"} />
+
+                                <div className={style["div-check-box"]}>
+
+                                    <CheckBox valor={"Casa"} />
+                                    <CheckBox valor={"Estúdio"} />
+
+                                </div>
+
+                                <InputDesabilitado valor={"Tamanho da vela"} />
+                                <InputDesabilitado valor={"Valor"} />
+
+                                <div className={style["div-text-area"]}>
+                                    <textarea name="" id="" disabled className={style["text-area"]}>fQIFGqo</textarea>
+                                </div>
+
+                            </div>
 
                             <div className='form-buttons'>
 
-                            <button className='cancel-button' onClick={vela}>Cancelar</button>
-                            <button className='confirm-button' onClick={vela}>Cadastrar</button>
+                                <button className='cancel-button' onClick={vela}>Cancelar</button>
+                                <button className='confirm-button' onClick={vela}>Cadastrar</button>
+
+                            </div>
+
 
                         </div>
 
-
                     </div>
 
+
                 </div>
-
-
             </div>
 
         </>
