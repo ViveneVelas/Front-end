b9f54e02dde0ab777be59bb930f7724b04ad5c68
Isa14-noronha <isabela.noronha@sptech.school>
2024-11-06 20:16:50 -0300

    Style: Mudança dos icones da sidebar para SVGs, e adição de hover aos SVGs

--- vivene-velas-frontend/src/components/sidebar/Sidebar.jsx
+++ vivene-velas-frontend/src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import dashIcon from '../../img/dashIcon.svg'
 import calendarioIcon from '../../img/calendarioIcon.svg'
-
+import estoqueIcon from '../../img/estoqueIcon.svg'
+import velaIcon from '../../img/velaIcon.svg'
+import pedidoIcon from '../../img/pedidoIcon.svg'
 import styles from './Sidebar.module.css'; // Importe o CSS Module
 
 const Sidebar = () => {
@@ -13,8 +15,8 @@ const Sidebar = () => {
   };
 
   const back = () => {
-      localStorage.clear()
-      window.location.href = '/login';
+    localStorage.clear()
+    window.location.href = '/login';
   }
 
   return (
@@ -26,45 +28,53 @@ const Sidebar = () => {
         <ul>
           <li>
             <a href="/dashboard">
-              <i><img src= {dashIcon} alt="" /></i>
+              <i><img src={dashIcon} alt="" /></i>
               {!collapsed && <span>Dashboard</span>}
             </a>
           </li>
+
           <li>
-            <a href="/calendario">
-              <i><img src= {calendarioIcon} alt="" /></i>
-              {!collapsed && <span>Agenda</span>}
+            <a href="/estoque">
+              <i><img src={estoqueIcon} alt="" /></i>
+              {!collapsed && <span>Estoque</span>}
             </a>
           </li>
+
+          <li>
+            <a href="/vela">
+              <i><img src={velaIcon} alt="" /></i>
+              {!collapsed && <span>Velas Padronizadas</span>}
+            </a>
+          </li>
+
+          <li>
+            <a href="/pedidos">
+              <i><img src={pedidoIcon} alt="" /></i>
+              {!collapsed && <span>Pedidos</span>}
+            </a>
+          </li>
+
+
           <li>
             <a href="/cadastro-velas">
               <i className="bi bi-plus-lg"></i>
               {!collapsed && <span>Adicionar Velas</span>}
             </a>
           </li>
+
           <li>
-            <a href="/estoque">
-              <i className="bi bi-boxes"></i>
-              {!collapsed && <span>Estoque</span>}
+            <a href="/calendario">
+              <i><img src={calendarioIcon} alt="" /></i>
+              {!collapsed && <span>Agenda</span>}
             </a>
-          </li>
-          <li>
-            <a href="/vela">
-              <i className="bi bi-clipboard-heart"></i>
-              {!collapsed && <span>Velas Padronizadas</span>}
-            </a>
-          </li>
-          <li>
-            <a href="/pedidos">
-            <i class="bi bi-clipboard-plus"></i>
-              {!collapsed && <span>Pedidos</span>}
-            </a>
-          </li>
-          <li onClick={back}>
-            <a>
-              <i className="bi bi-box-arrow-right"></i>
-              {!collapsed && <span>Sair</span>}
-            </a>
+
+            <li onClick={back}>
+              <a>
+                <i className="bi bi-box-arrow-right"></i>
+                {!collapsed && <span>Sair</span>}
+              </a>
+            </li>
+
           </li>
         </ul>
       </nav>
