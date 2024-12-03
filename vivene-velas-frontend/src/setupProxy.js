08f1357eb27eb37fab1e3e227bee050f93ef28b5
paulo07marcena <paulo.marcena@sptech.school>
2024-12-03 01:50:12 -0300

    feat: Update to use proxy

--- vivene-velas-frontend/src/setupProxy.js
+++ vivene-velas-frontend/src/setupProxy.js
@@ -2,11 +2,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-        '/host',
-        createProxyMiddleware({
-            target: 'http://98.84.38.39:8080', 
-            changeOrigin: true, 
-        })
-    );
+  app.use(
+    '/host',  // Quando o frontend fizer uma requisição para /host, será redirecionado
+    createProxyMiddleware({
+      target: 'http://54.82.254.93:8080',  // O IP e a porta do seu backend
+      changeOrigin: true,  // Modifica o cabeçalho "Origin" para o target (backend)
+      pathRewrite: {
+        '^/host': '',  // Remove o /host do caminho antes de encaminhar para o backend
+      },
+    })
+  );
 };
