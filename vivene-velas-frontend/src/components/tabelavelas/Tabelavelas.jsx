c3a6e7c18872df69b3f77655df0b17c47b641450
babi-barbato <barbara.oliveira@sptech.school>
2024-12-03 17:02:42 -0300

    fix: ajustes no cad de pedido

--- vivene-velas-frontend/src/components/tabelavelas/Tabelavelas.jsx
+++ vivene-velas-frontend/src/components/tabelavelas/Tabelavelas.jsx
@@ -3,56 +3,62 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const TabelaPedidos = () => {
-    const [dados, setDados] = useState([]);
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const pedidoResponse = await axios.get('http://localhost:8080/pedidos/top-cinco-pedidos', {
-            headers: {
-              'accept': '*/*',
-            },
-          });
-    
-          const responseData = pedidoResponse.data;
-          
-          // Coloca o objeto dentro de um array
-          const dataArray = Array.isArray(responseData) ? responseData : [responseData];
+  const [dados, setDados] = useState([]);
 
-          setDados(dataArray);
-        } catch (error) {
-          console.error('Erro ao buscar os dados:', error);
-        }
-      };
-    
-      fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const pedidoResponse = await axios.get('http://localhost:8080/pedidos/top-cinco-pedidos', {
+          headers: {
+            'accept': '*/*',
+          },
+        });
 
-    return (
-        <>
-            <div className="table-container card">
-                <h5 className="">Pedidos próximos ao vencimento</h5>
-                <table className="tabela-vendas">
-                    <thead>
-                        <tr>
-                            <th scope="col">Data</th>
-                            <th scope="col">Nome do Cliente</th>
-                            <th scope="col">Quantidade de Velas</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {dados.map((item, index) => (
-                            <tr key={index}>
-                                <td>{item.dataPedido}</td>
-                                <td>{item.nomeCliente}</td>
-                                <td>{item.qtdVelas}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    );
+        const responseData = pedidoResponse.data;
+
+        // Coloca o objeto dentro de um array
+        const dataArray = Array.isArray(responseData) ? responseData : [responseData];
+
+        setDados(dataArray);
+      } catch (error) {
+        console.error('Erro ao buscar os dados:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  function formatarData(date) {
+    const [year, month, day] = date.split("-");
+    return `${day}/${month}/${year}`;
+  }
+
+  return (
+    <>
+      <div className="table-container card">
+        <h5 className="">Pedidos próximos ao vencimento</h5>
+        <table className="tabela-vendas">
+          <thead>
+            <tr>
+              <th scope="col">Data</th>
+              <th scope="col">Nome do Cliente</th>
+              <th scope="col">Quantidade de Velas</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dados.map((item, index) => (
+              <tr key={index}>
+                <td>{formatarData(item.dataPedido)}</td>
+                <td>{item.nomeCliente}</td>
+                <td>{item.qtdVelas}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default TabelaPedidos;
