a70477ff8fc6c76a07473993dd682895fadd04a3
paulo07marcena <paulo.marcena@sptech.school>
2024-12-03 16:56:52 -0300

    feat: Update IP to backend

--- vivene-velas-frontend/src/setupProxy.js
+++ vivene-velas-frontend/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/host',  // Quando o frontend fizer uma requisição para /host, será redirecionado
     createProxyMiddleware({
-      target: 'http://54.82.254.93:8080',  // O IP e a porta do seu backend
+      target: 'http://54.236.201.0:8080',  // O IP e a porta do seu backend
       changeOrigin: true,  // Modifica o cabeçalho "Origin" para o target (backend)
       pathRewrite: {
         '^/host': '',  // Remove o /host do caminho antes de encaminhar para o backend
